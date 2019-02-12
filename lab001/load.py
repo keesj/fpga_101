6872af063a0ade91dfaf1290ac2f963858510dca
Kees Jongenburger <kees.jongenburger@gmail.com>
2019-02-12 10:29:21 +0100

    Blink on the papilio pro

--- lab001/load.py
+++ lab001/load.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 import os
-os.system("djtgcfg prog -d Nexys4DDR -i 0 -f ./build/top.bit")
+# The papilio pro comes with an ide and a fork of xc3sprog called papilio_prog
+# https://github.com/GadgetFactory/Papilio-Loader/tree/master/xc3sprog/trunk/bscan_spi
+#
+#
+# xc3sprog was cloned from https://github.com/matrix-io/xc3sprog.git
+# patched with 858454a04ba8cf87125982beca666286b9f63362 ([PATCH] fix WiringPI API dependency)
+# 
+os.system("xc3sprog -v -c papilio build/top.bit")
